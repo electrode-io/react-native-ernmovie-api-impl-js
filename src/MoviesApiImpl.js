29b62178090d82fa4ea03e38386f658040144910
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-11-16 17:00:06 -0800

    initial commit

---
+++ src/MoviesApiImpl.js
@@ -0,0 +1,23 @@
+import { MoviesApi } from 'react-native-ernmovie-api'
+
+MoviesApi.requests().registerGetTopRatedMoviesRequestHandler((requestData) => {
+  let movies = [{
+    title: 'TitanicJS',
+    releaseYear: 1997,
+    ratings: '4.5',
+    imageUrl: 'http://bit.ly/2hnU8mq',
+    description: 'Titanic'
+  }, {
+    title: 'AvatarJS',
+    releaseYear: 2009,
+    ratings: '4.0',
+    imageUrl: 'http://bit.ly/2xAX0Cv',
+    description: 'Avatar'
+  }]
+  Promise.resolve(movies)
+})
+
+MoviesApi.requests().registerGetMovieDetailRequestHandler((requestData) => {
+  //TODO: Write your implementation and resolve or reject the promise
+  //Promise.resolve(result) || Promise.reject(err)
+})
